f03b784ebc0a671c183f65d154c3a1a5647a550e
MineFX <realminefx@gmail.com>
2021-09-15 17:40:56 -0400

    Initial commit

---
+++ Alpine Level Editor/src/Alpine.cs
@@ -0,0 +1,50 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+using TGUI;
+
+namespace Alpine_Level_Editor {
+    public class Alpine {
+        private RenderWindow window;
+        private VideoMode mode;
+
+        private Gui gui;
+        private Sprite sprite;
+
+        public Alpine() {
+        }
+
+        public bool isRunning() {
+            return window.IsOpen;
+        }
+
+        public void init() { 
+            mode = new VideoMode(1280, 720);
+            window  = new RenderWindow(mode, "Alpine Level Editor");
+            gui = new Gui(window);
+        }
+
+        public void create() {
+            gui.LoadWidgetsFromFile("res/fuck.txt");
+        }
+
+        public void update() {
+            events();
+        }
+
+        public void events() {
+            window.DispatchEvents();
+            
+            window.Closed += (sender, e) => {
+                ((Window) sender).Close();
+            };
+        }
+
+        public void render() {
+            window.Clear();
+            
+            gui.Draw();
+            
+            window.Display();
+        }
+    }
+}
---
+++ Alpine Level Editor/src/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Alpine_Level_Editor {
+    class Program {
+        static void Main(string[] args) {
+            Alpine alpine = new Alpine();
+
+            alpine.init(); //Create window and shit.
+            alpine.create(); //Create sprites and shit.
+
+            while (alpine.isRunning()) {
+                alpine.update(); //Update shit.
+                alpine.render(); //Render shit.
+            }
+        }
+    }
+}
