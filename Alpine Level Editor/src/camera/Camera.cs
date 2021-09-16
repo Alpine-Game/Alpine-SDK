96ac58a3422f12c35742e2eb404047b74a5b787f
MineFX <realminefx@gmail.com>
2021-09-15 20:35:48 -0400

    Add basic input handling and viewport movement.

---
+++ Alpine Level Editor/src/camera/Camera.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Alpine_Level_Editor.input;
+using SFML.Graphics;
+using SFML.System;
+
+namespace Alpine_Level_Editor.camera {
+    public class Camera {
+        private View view;
+
+        public Camera(View view) {
+            this.view = view;
+        }
+
+        public void update() {
+            float horiz = Input.getHorizontal();
+            float verti = Input.getVertical();
+
+            float velocityX = 0f;
+            float velocityY = 0f;
+
+            float speed = 160;
+
+            if (horiz != 0) {
+                velocityX = view.Center.X + horiz * speed * Program.alpine.deltaTime;
+            }
+            else {
+                velocityX = view.Center.X;
+            }
+            
+            if (verti != 0) {
+                velocityY = view.Center.Y + verti * speed * Program.alpine.deltaTime;
+            }
+            else {
+                velocityY = view.Center.Y;
+            }
+
+            view.Center = new Vector2f(velocityX, velocityY);
+            
+            Console.WriteLine(view.Center.X);
+        }
+    }
+}
---
+++ Alpine Level Editor/src/input/Input.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Alpine_Level_Editor.input {
+    public class Input {
+        private static bool up = false;
+        private static bool down = false;
+        private static bool left = false;
+        private static bool right = false;
+
+        private static bool listenerActive = false;
+
+        public static float getHorizontal() {
+            if (listenerActive) {
+                if (left && right) return 0;
+                if (left) return -1;
+                if (right) return 1;
+            }
+            
+            return 0;
+        }
+        public static float getVertical() {
+            if (listenerActive) {
+                if (up && down) return 0;
+                if (down) return -1;
+                if (up) return 1;
+            }
+
+            return 0;
+        }
+
+        public static void listenForEvents(RenderWindow window) {
+            listenerActive = true;
+            
+            window.KeyPressed += (sender, e) => {
+                switch (e.Code) {
+                    case Keyboard.Key.W:
+                        up = true;
+                        break;
+                    case Keyboard.Key.S:
+                        down = true;
+                        break;
+                    case Keyboard.Key.A:
+                        left = true;
+                        break;
+                    case Keyboard.Key.D:
+                        right = true;
+                        break;
+                }
+            };
+            
+            window.KeyReleased += (sender, e) => {
+                switch (e.Code) {
+                    case Keyboard.Key.W:
+                        up = false;
+                        break;
+                    case Keyboard.Key.S:
+                        down = false;
+                        break;
+                    case Keyboard.Key.A:
+                        left = false;
+                        break;
+                    case Keyboard.Key.D:
+                        right = false;
+                        break;
+                }
+            };
+        }
+    }
+}
