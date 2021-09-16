a19847ff4725301d1cb0481598d0fc7becedbff5
MineFX <realminefx@gmail.com>
2021-09-15 20:36:09 -0400

    Program class shit nothing to see here all it does is just... ya know.. runs the game.

--- Alpine Level Editor/src/Program.cs
+++ Alpine Level Editor/src/Program.cs
@@ -4,8 +4,9 @@ using SFML.Window;
 
 namespace Alpine_Level_Editor {
     class Program {
+        public static Alpine alpine;
         static void Main(string[] args) {
-            Alpine alpine = new Alpine();
+             alpine = new Alpine();
 
             alpine.init(); //Create window and shit.
             alpine.create(); //Create sprites and shit.
